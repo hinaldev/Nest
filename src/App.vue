ab478e76fef96560f61e072e74b6496ff9ee6761
Hindy Albert <Tichnut14@8547900.org.il>
2025-07-27 18:05:20 +0300

    finish learn

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1 class="title">Users List</h1>
+    <ul class="user-list">
+      <li v-for="user in users" :key="user.id" class="user-item">
+        <div class="user-card">
+          <h3 class="user-name">{{ user.name }}</h3>
+          <p class="user-email">{{ user.email }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/users')
+      .then(response => response.json())
+      .then(data => {
+        this.users = data;
+      })
+      .catch(error => console.log(error));
+  }
+};
+</script>
+
+<style src="./assets/styles.css"></style>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+const webpack = require('webpack');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
+        // הוסף דגלי תכונה נוספים אם יש צורך
+      }),
+    ],
+  },
+};
